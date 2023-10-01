a9d40ab68cfceeee492caa98270e6458105ff724
vdd-bandara <133400052+vdd-bandara@users.noreply.github.com>
2023-10-02 00:55:06 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+
+</head>
+
+<body>
+
+    <div class="col-12 col-lg-12">
+        <div class="raw">
+
+            <div class="col-12 ">
+                <div class="row">
+
+                    <div class="col-12 mt-4 mb-2">
+                        <div class="raw">
+
+                            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                                <div class="container-fluid">
+                                    <a class="navbar-brand  offset-1"><img class="logo" src="resources/logo.png" alt=""></a>
+                                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                    </button>
+                                    <div class="collapse navbar-collapse offset-1" id="navbarSupportedContent">
+                                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                            <li class="nav-item">
+                                                <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">PAGES</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">BLOG</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">GALLARY</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">ELEMENT</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">SHOP</a>
+                                            </li>
+                                            <li class="nav-item offset-5">
+                                                <a class="nav-link" href="#"><i class="bi bi-search"></i></a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#"><i class="bi bi-cart3"></i></a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <button class="btn btn-transparent d-flex" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="bi bi-menu-button"></i></button>
+
+                                                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                                                    <div class="offcanvas-header">
+                                                        <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="offcanvas-body">
+                                                        
+                                                    </div>
+                                                </div>
+
+                                            </li>
+                                        </ul>
+
+                                        <!-- <form class="d-flex" role="search">
+                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="btn btn-outline-success " type="submit">Search</button>
+                                </form> -->
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </nav>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <script src="bootstrap.bundle.js"></script>
+</body>
---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> Farm Organic Store</title>
+
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="stylesheet" href="style.css" />
+
+</head>
+
+<body>
+
+    <div class="container-fluid">
+
+        <div class="raw">
+
+            <?php include "header.php"; ?>
+
+            <div class="col-12">
+
+                <!-- carousal -->
+
+                <div class="col-12">
+                    <div class="row">
+
+                        <div id="carouselExample" class="carousel slide p-0" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="resources/slide-1.jpg" class="d-block w-100">
+                                    <div class="carousel-caption d-none d-md-block caption">
+                                        <h4 style="font-weight: bold;">WELCOME TO OUR FARM</h4>
+                                        <p class="p">PRODUCTS</p>
+                                        <h1 style="font-weight:bold;">GROWN WITH LOVE</h1>
+                                        <button type="button" class="btn btn-warning button">Shop Now</button>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="resources/slide-2.jpg" class="d-block w-100">
+                                    <div class="carousel-caption d-none d-md-block caption">
+                                        <h4 style="font-weight: bold;">HIGH QUALITY ORGANIC PRODUCTS</h4>
+                                        <p class="p">VEGETABLES</p>
+                                        <h1 style="font-weight:bold;">WITHOUT HARMFULL ADDITIVES</h1>
+                                        <button type="button" class="btn btn-warning button">Shop Now</button>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="resources/slide-3.jpg" class="d-block w-100">
+                                    <div class="carousel-caption d-none d-md-block caption">
+                                        <h4 style="font-weight: bold;">FRESH ORGANIC PRODUCE</h4>
+                                        <p class="p">FRUITS</p>
+                                        <h1 style="font-weight:bold;">WITH LOT OF VITAMIS</h1>
+                                        <button type="button" class="btn btn-warning button">Shop Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
+                <!-- carousal -->
+
+                <!-- about us -->
+
+                <div class="col-12">
+                    <div class="row raw-30 align-items-center justify-content-center justify-content-xl-between">
+
+                        <div class="col-6 col-lg-6 col-md-5 ">
+
+                            <img src="resources/about.jpg" style="width: 576px; height:410px;">
+
+                        </div>
+
+                        <div class="col-6 col-lg-6 col-md-7">
+
+                            <h4 class="t1">A Few Words About Our Farm</h4>
+
+                            <h2 class="t2">ABOUT US</h2>
+
+                            <p class="offset-top-md-20">Farm is made up of two certified organic properties,where our farmers are constatly growing organic
+                                vegetables, crops and fruits of the highest quality in the US</p>
+
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="row">
+                                            <div class="col-3">
+                                                <button type="button" class="btn btn-outline-warning b d-flex flex-row align-items-center justify-content-center"><i class="bi bi-arrow-right-short fs-4" style="color: black;"></i></button>
+
+                                            </div>
+                                            <div class="col-9 d-flex flex-column justify-content-start">
+
+                                                <span class="s">sam williams</span>
+                                                <span class="f">Farm Founder</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-8 d-flex flex-row justify-content-start ">
+                                        <img src="resources/signature.png" class="i offset-1">
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <!-- about us -->
+
+                <!-- services -->
+
+                <div class="col-12 col-lg-12 ">
+                    <div class="row">
+                        <div class="col-12 pg d-flex flex-row align-items-center">
+                            <div class="row  col-md-10 col-lg-10 offset-0 offset-md-1 offset-lg-1 ps-4 pe-4 pt-2 pb-2 ">
+                                
+                                <div class="col-12 ">
+                                    <div class="row  ">
+                                        <div class="col-3 ">
+                                            <div class="col-12">
+                                                <div class="row">
+                                                    <h4 class="text-white">ALWAYS FRESH</h4>
+                                                    <p class="pb">Thanks to innovative organic technologies, our products are always fresh and don’t need any treatment.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-3 ">
+                                            <div class="col-12">
+                                                <div class="row">
+                                                    <h4 class="text-white">100% ORGANIC</h4>
+                                                    <p class="pb">We adhere to our organic philosophy, which implies avoiding pesticides and non-organic fertilizers.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-3 ">
+                                            <div class="col-12">
+                                                <div class="row">
+                                                    <h4 class="text-white">NO ADDITIVES</h4>
+                                                    <p class="pb">We avoid any artificial additives after harvesting the crops to offer you the best organic products.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-3">
+                                            <div class="col-12">
+                                                <div class="row">
+                                                    <h4 class="text-white">MORDEN FARM</h4>
+                                                    <p class="pb">Though we rely on traditional organic farming a lot, we also use the latest farming innovations.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- services -->
+
+                <!-- Products -->
+
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12">
+                            <h4 class="l text-center">Latest Arrival</h4>
+
+                            <h2 class="text-center n">New Products</h2>
+
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Products -->
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <script src="bootstrap.bundle.js"></script>
+    <script src="bootstrap.js"></script>
+    <script src="script.js"></script>
+
+</body>
