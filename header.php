3d7b9483b5af9956d4788c75f55b428c87acce6d
vdd-bandara <133400052+vdd-bandara@users.noreply.github.com>
2023-10-04 22:18:17 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -1,89 +1,84 @@
 <!DOCTYPE html>
-<html>
 
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> Sample</title>
 
     <link rel="stylesheet" href="bootstrap.css" />
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
-
 </head>
 
 <body>
 
-    <div class="col-12 col-lg-12">
-        <div class="raw">
+    <div class="col-12">
+        <div class="row">
 
-            <div class="col-12 ">
+            <div class="col-2">
                 <div class="row">
+                    <div class="col-12">
+                        <img class="lg d-flex align-items-center mt-3 mb-3 offset-0 offset-lg-3" src="resources/images/logo.png">
+                    </div>
+                </div>
+            </div>
 
-                    <div class="col-12 mt-4 mb-2">
-                        <div class="raw">
-
-                            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                                <div class="container-fluid">
-                                    <a class="navbar-brand  offset-1"><img class="logo" src="resources/logo.png" alt=""></a>
-                                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                        <span class="navbar-toggler-icon"></span>
-                                    </button>
-                                    <div class="collapse navbar-collapse offset-1" id="navbarSupportedContent">
-                                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                            <li class="nav-item">
-                                                <a class="nav-link active" aria-current="page" href="#">HOME</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="#">PAGES</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="#">BLOG</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="#">GALLARY</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="#">ELEMENT</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="#">SHOP</a>
-                                            </li>
-                                            <li class="nav-item offset-5">
-                                                <a class="nav-link" href="#"><i class="bi bi-search"></i></a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="#"><i class="bi bi-cart3"></i></a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <button class="btn btn-transparent d-flex" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="bi bi-menu-button"></i></button>
-
-                                                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                                                    <div class="offcanvas-header">
-                                                        <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="offcanvas-body">
-                                                        
-                                                    </div>
-                                                </div>
-
-                                            </li>
-                                        </ul>
-
-                                        <!-- <form class="d-flex" role="search">
-                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                    <button class="btn btn-outline-success " type="submit">Search</button>
-                                </form> -->
-                                        <span></span>
-                                    </div>
-                                </div>
-                            </nav>
-
+            <div class="col-5">
+                <div class="row">
+                    <div class="col 12">
+                        <div class="row">
+                            <ul class="nav mt-3 offset-lg-4">
+                                <li class="nav-item">
+                                    <a class="nav-link active fs-5" style="color: black;" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fs-5" style="color: black;" href="#">Our Service</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fs-5" style="color: black;" href="#">Our Team</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fs-5" style="color: black;" href="#">Contact Us</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
+                </div>
+            </div>
 
+            <div class="col-5">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="offset-lg-4 col-12 col-lg-3 align-self-end mt-3">
+                            <div class="row">
+
+                                <div class="col-1 col-lg-3 ms-5 ms-lg-0 mt-1 cart-icon"></div>
+
+                                <div class="col-2 col-lg-6 offset-lg-2">
+                                    <div class="col-12">
+                                        <button class="btn btn-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="bi bi-list"></i></button>
+
+                                        <div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                                            <div class="offcanvas-header">
+                                                <h4 class="offcanvas-title text-light" id="offcanvasRightLabel">My Cake Shop</h4>
+                                                <button type="button" class="btn-close bg-light" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                            </div>
+                                            <div class="offcanvas-body">
+                                                <H5 class="text-light offset-1 mt-4 mb-4  signout" onclick="window.location='userProfile.php'">My Profile</H5>
+                                                <H5 class="text-light offset-1 mt-4 mb-4  signout" onclick="window.location=''">My Product</H5>
+                                                <H5 class="text-light offset-1 mt-4 mb-4  signout" onclick="window.location=''">Watchlist</H5>
+                                                <H5 class="text-light offset-1 mt-4 mb-4  signout" onclick="window.location=''">Purchase History</H5>
+                                                <H5 class="text-light offset-1 mt-4 mb-4  signout" onclick="window.location=''">Message</H5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -91,4 +86,5 @@
     </div>
 
     <script src="bootstrap.bundle.js"></script>
+    <script src="bootstrap.js"></script>
 </body>
