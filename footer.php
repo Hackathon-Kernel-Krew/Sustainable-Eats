d15b4f3ab469898a6b51187779d3ae5f28e20584
vdd-bandara <133400052+vdd-bandara@users.noreply.github.com>
2023-10-04 19:47:07 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> Sample</title>
+
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+</head>
+
+<body>
+
+    <footer>
+
+        <div class="col-12 col-lg-12 ">
+            <div class="row">
+                <div class="col-12 pg d-flex flex-row align-items-center">
+                    <div class="row  col-md-10 col-lg-10 offset-0 offset-md-1 offset-lg-1 ps-4 pe-4 pt-2 pb-2 ">
+
+                        <div class="col-12 ">
+                            <div class="row  ">
+                                <div class="col-4 ">
+                                    <div class="col-12">
+                                        <div class="row">
+                                            <img class="logo" src="resources/images/logo.png">
+                                            <!-- <h4 class="text-white">ALWAYS FRESH</h4>
+                                        <p class="pb">Thanks to innovative organic technologies, our products are always fresh and don’t need any treatment.</p> -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4 ">
+                                    <div class="col-12">
+                                        <div class="row">
+                                            <h5 class="text-uppercase text-light mb-4">get social</h5>
+                                            <ul class="list-unstyled list-inline">
+
+                                                <li class="list-inline-item">
+                                                    <a href="#" class="form-floating ">
+                                                        <i class="bi bi-facebook" style="font-size: 22px; color:black;"></i>
+                                                    </a>
+                                                </li>
+
+                                                <li class="list-inline-item">
+                                                    <a href="#" class="form-floating ">
+                                                        <i class="bi bi-twitter" style="font-size: 22px; color:black;"></i>
+                                                    </a>
+                                                </li>
+
+                                                <li class="list-inline-item">
+                                                    <a href="#" class="form-floating ">
+                                                        <i class="bi bi-whatsapp" style="font-size: 22px; color:black;"></i>
+                                                    </a>
+                                                </li>
+
+                                                <li class="list-inline-item">
+                                                    <a href="#" class="form-floating ">
+                                                        <i class="bi bi-linkedin" style="font-size: 22px; color:black;"></i>
+                                                    </a>
+                                                </li>
+
+                                                <li class="list-inline-item">
+                                                    <a href="#" class="form-floating ">
+                                                        <i class="bi bi-youtube" style="font-size: 22px; color:black;"></i>
+                                                    </a>
+                                                </li>
+
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4 ">
+                                    <div class="col-12">
+                                        <div class="row">
+                                            <h5 class="text-uppercase text-light mb-4">Contact Us</h5>
+
+                                            <p><i class="bi bi-geo-alt-fill">&nbsp;</i>Maradana, Colombo 10, Sri Lanka</p>
+                                            <p><i class="bi bi-envelope-fill">&nbsp;</i>farm@gmail.com</p>
+                                            <p><i class="bi bi-telephone-fill">&nbsp;</i>+94112 4445558</p>
+                                            <p><i class="bi bi-printer-fill">&nbsp;</i>+94112 4445558</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <hr class="mb-4 " />
+                                </div>
+
+                                <div class="col-12 col-md-3 col-lg-4 mx-auto mt-3">
+                                    <p>copyright &copy; 2023 farm All rights reserved</p>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+
+    <script src="bootstrap.bundle.js"></script>
+    <script src="bootstrap.js"></script>
+</body>
