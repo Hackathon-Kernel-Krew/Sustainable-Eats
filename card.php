44672b5e149883e39b7c5e82d5b179e7dbc1f58c
vdd-bandara <133400052+vdd-bandara@users.noreply.github.com>
2023-10-04 13:00:48 +0530

    Add files via upload

---
+++ card.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> Sample</title>
+
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+</head>
+
+<body>
+
+    <div class="col-12">
+        <div class="row">
+
+            <div class="row cd">
+                <div class="card ps-3 pe-3 pt-4 pb-4 mt-2  d-flex align-items-center border border-0 " style="width: 18rem; background-color: lightgray; border-radius:30px;">
+                    <img src="images/product.png" class="card-img-top img">
+                    <div class="card-body">
+                        <h5 class="card-title">BANANAS &nbsp;&nbsp;&nbsp;<span class="badge bg-info">New</span></h5>
+                        <p class="card-text ">Rs.100</p>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-center mt-2 mb-2">
+                            <button class="btn btn-light me-md-2 button d-flex flex-row align-items-center justify-content-center" type="button"><i class="bi bi-cart-plus fs-4" style="color: black;"></i></button>
+                            <button class="btn btn-light me-md-2 button d-flex flex-row align-items-center justify-content-center" type="button"><i class="bi bi-heart fs-4" style="color: black;"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <script src="bootstrap.bundle.js"></script>
+    <script src="bootstrap.js"></script>
+</body>
